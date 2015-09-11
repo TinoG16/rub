499df3a8660d1e2584b638bab885e22d2dad4b5b
16max <schelemax@g16hh.de>
2015-09-11 11:24:46 +0200

    Methoden & Klassen

--- cod/wor/klassen.rb
+++ cod/wor/klassen.rb
@@ -4,10 +4,20 @@ class Movie
 		@rank = prank
 	end
 
+	def thumbs_up
+		@rank += 1
+	end
+
+	def thumbs_down
+		@rank -= 1
+	end
+
 	def to_s
 		"#{@titel} hat ein Ranking von: #{@rank}."
 	end
 end
 
 obj_movie1 = Movie.new("Ninja Turtles", 4)
+puts obj_movie1.to_s
+obj_movie1.thumbs_up
 puts obj_movie1.to_s
